eeed462c354786dfaef74aae546f9b1e1ac19c03
9832188947 <shreyakmukherjeedgp@gmail.com>
2024-07-23 13:16:55 +0530

    Added my project

---
+++ chatapplication/src/App.jsx
@@ -0,0 +1,12 @@
+//import SignIn from "./components/signin";
+//import SignUp from "./components/signup";
+import MainPage from "./components/mainpage";
+function App() {
+  return (
+    <>
+      <MainPage />
+    </>
+  );
+}
+
+export default App;
